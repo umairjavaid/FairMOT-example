09ecc864a26e49ef3e3b09253e3512adc3fdfbb2
umairjavaid <bscs13024@itu.edu.pk>
2021-05-25 17:39:40 +0500

    Update img_preprocess.py

--- img_preprocess.py
+++ img_preprocess.py
@@ -1,1 +1,18 @@
+from PIL import Image
+from torchvision import transforms
 
+def convert_to_PIL(img):
+  img = Image.fromarray(img)
+  return img
+
+data_transforms = transforms.compose([
+  transforms.ToTensor(),
+  transforms.Resize(224),
+  transforms.Normalize([0.485, 0.456, 0.406],[0.299, 0.244, 0.225])
+
+def makeImgModelReady(img):
+  global data_transforms
+  img = convert_to_PIL(img)
+  img = data_transforms(img)
+  img = torch.unsqueeze(img, 0)
+  return img
