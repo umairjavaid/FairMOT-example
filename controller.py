ddf48c32577825646d3bd4cfa8dcefedb7007605
umairjavaid <bscs13024@itu.edu.pk>
2021-05-25 17:30:21 +0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,3 +4,7 @@ from load_fairmot_weights import load_fairmot_weights
 model = get_pose_net(34, heads={'hm': 1, 'wh': 4, 'id': 128, 'reg': 2}, head_conv=256)
 weights_path = "/content/FairMOT/models/crowdhuman_dla34.pth"
 model = load_fairmot_weights(model, weights_path)
+img_path = ""
+img = load_img(img_path)
+img = preprocess_img(img)
+print(model(img))
