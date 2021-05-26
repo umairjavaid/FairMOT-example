3585273abdd9319231f1bd75a272523bd22a061b
umairjavaid <bscs13024@itu.edu.pk>
2021-05-26 11:39:14 +0500

    Update img_preprocess.py

--- img_preprocess.py
+++ img_preprocess.py
@@ -5,10 +5,11 @@ def convert_to_PIL(img):
   img = Image.fromarray(img)
   return img
 
-data_transforms = transforms.compose([
+data_transforms = transforms.Compose([
   transforms.ToTensor(),
   transforms.Resize(224),
   transforms.Normalize([0.485, 0.456, 0.406],[0.299, 0.244, 0.225])
+  ])
 
 def makeImgModelReady(img):
   global data_transforms
