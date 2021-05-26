b372cde14ec454283611fc3840aecfe3dd3ff3d7
umairjavaid <bscs13024@itu.edu.pk>
2021-05-26 13:48:54 +0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 model = get_pose_net(34, heads={'hm': 1, 'wh': 4, 'id': 128, 'reg': 2}, head_conv=256)
 weights_path = "/content/FairMOT/models/crowdhuman_dla34.pth"
 model = load_fairmot_weights(model, weights_path)
+model = model.cuda()
 img_path = "/content/FairMOT-example/cover4.jpg"
 
-img = plt.imread(img_path)
-print(img.shape)
-
-img = makeImgModelReady(img)
-
+img = fairmot_transform(img_path)
 print(model(img))
+
+#img = plt.imread(img_path)
+#print(img.shape)
+
+#img = makeImgModelReady(img)
+
+#print(model(img))
